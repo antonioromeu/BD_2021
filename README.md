884176f4e201367dd9da1bd6d9e4fbc03203a91a
antonioromeu <antonioromeu@tecnico.ulisboa.pt>
2020-11-16 14:26:55 +0000

    minor changes

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 
 ### duvidas
 
-<ul>
-<li> PRIMARY KEY (num_regiao), ou CONSTRAINT pk_regiao PRIMARY KEY(num_regiao) </li>
-<li> pqq nas constraints de keys paree que temos de renomear as colunas </li>
 <li> qual o tamanho dos varchars? </li>
-<li> se houver duas foreign keys da mesma tabela pomos na mesma constraint? </li>
 <li> a quantidade nas prescrições é decimal/int/varchar </li>
+<li> PRIMARY KEY (num_regiao), ou CONSTRAINT pk_regiao PRIMARY KEY(num_regiao) </li>
+<ul>
+<li> pqq nas constraints de keys paree que temos de renomear as colunas </li>
+<li> se houver duas foreign keys da mesma tabela pomos na mesma constraint? </li>
 <li> FK de uma mesma tabela podem ter o mesmo nome? </li>
 <li> </li>
 </ul>
--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE regiao (
     nome VARCHAR(40) NOT NULL
     num_habitantes INT NOT NULL,
     PRIMARY KEY (num_regiao)
-    CONSTRAINT nome
+    CONSTRAINT RI-regiao-1
         CHECK nome IN {'Norte', 'Centro', 'Lisboa', 'Alentejo', 'Algarve'},
     -- CONSTRAINT pk_regiao PRIMARY KEY(num_regiao)
 );
@@ -14,9 +14,7 @@ CREATE TABLE concelho (
     nome VARCHAR(40) NOT NULL,
     num_habitantes INT NOT NULL,
     PRIMARY KEY (num_concelho),
-    CONSTRAINT fk_concelho
-        FOREIGN KEY (num_regiao)
-        REFERENCES regiao(num_regiao)
+    FOREIGN KEY (num_regiao) REFERENCES regiao(num_regiao)
 );
 
 CREATE TABLE instituicao (
@@ -24,9 +22,9 @@ CREATE TABLE instituicao (
     tipo VARCHAR(40) NOT NULL,
     num_regiao INT NOT NULL,
     num_concelho INT NOT NULL,
-    CONSTRAINT fk_instituicao
-        FOREIGN KEY (num_regiao, num_concelho)
-        REFERENCES concelho(num_regiao, num_concelho)
+    FOREIGN KEY (num_regiao, num_concelho) REFERENCES concelho(num_regiao, num_concelho)
+    CONSTRAINT RI-instituicao-1
+        CHECK tipo IN {'farmacia', 'laboratorio', 'clinica', 'hospital'}
 );
 
 CREATE TABLE medico (
@@ -39,26 +37,21 @@ CREATE TABLE medico (
 CREATE TABLE consulta (
     num_cedula INT NOT NULL,
     num_doente INT NOT NULL,
-    data_consulta INT NOT NULL, 
+    data_consulta INT NOT NULL,
     nome_instituicao VARCHAR(40) NOT NULL,
     PRIMARY KEY(data_consulta),
-    CONSTRAINT fk_consulta
-        FOREIGN KEY (num_cedula)
-        REFERENCES medico(num_cedula),
-    CONSTRAINT fk_consulta
-        FOREIGN KEY (num_doente)
-        REFERENCES instituicao(num_doente),
-    -- CONSTRAINT data_consulta CHECK DATEPART(weekday, data_consulta) != 7
-    CONSTRAINT data_consulta
-        CHECK EXTRACT(WEEKDAY FROM data_consulta) IN (1, 2, 3, 4, 5)
+    FOREIGN KEY (num_cedula) REFERENCES medico(num_cedula),
+    FOREIGN KEY (num_doente) REFERENCES instituicao(num_doente),
+    CONSTRAINT RI-consulta-1
+        CHECK EXTRACT(DOW FROM data_consulta) IN (1, 2, 3, 4, 5)
+    CONSTRAINT RI-consulta-2
+        CHECK (SELECT COUNT(num_doente) FROM consulta WHERE
+        SELECT COUNT(*) FROM (
+            -- SELECT (num_doente = consulta(num_doente), data_consulta = consulta(data_consulta), nome_instituicao = ) FROM consulta
+        )
+
+
 );
-
--- lucia
--- https://cloud.google.com/bigquery/docs/reference/standard-sql/date_functions
-
--- https://www.w3schools.com/sql/sql_check.asp
-
--- https://stackoverflow.com/questions/9570087/how-to-check-if-datetime-happens-to-be-saturday-or-sunday-in-sql-server-2008
 
 
 CREATE TABLE prescricao (
@@ -68,9 +61,8 @@ CREATE TABLE prescricao (
     substancia VARCHAR(40) NOT NULL,
     quantidade DECIMAL NOT NULL,
     PRIMARY KEY (num_cedula, num_doente, data_prescricao, substancia),
-    CONSTRAINT fk_prescricao
-        FOREIGN KEY (num_cedula, num_doente, data_prescricao)
-        REFERENCES consulta(num_cedula, num_doente, data_prescricao)
+    FOREIGN KEY (num_cedula, num_doente, data_prescricao)
+    REFERENCES consulta(num_cedula, num_doente, data_prescricao)
 );
 
 CREATE TABLE analise (
@@ -84,13 +76,9 @@ CREATE TABLE analise (
     quant DECIMAL NOT NULL,
     inst VARCHAR(40) NOT NULL,
     PRIMARY KEY (num_analise),
-    CONSTRAINT fk_analise_consulta
-        FOREIGN KEY (num_cedula, num_doente, data_analise)
-        REFERENCES consulta(num_cedula, num_analise, data_consulta),
-    CONSTRAINT fk_analise_instituicao
-        FOREIGN KEY (inst)
-        REFERENCES instituicao(nome)
-    CONSTRAINT analise_especialidade DEFAULT
+    FOREIGN KEY (num_cedula, num_doente, data_analise) REFERENCES consulta(num_cedula, num_analise, data_consulta),
+    FOREIGN KEY (inst) REFERENCES instituicao(nome)
+    CONSTRAINT RI-analise DEFAULT
         SELECT especialidade FROM medico WHERE num_cedula = medico(num_cedula)
 );
 
