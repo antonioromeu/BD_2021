8022fa3d861d02cea9d0ddaaf54e9b3c8e809233
antonioromeu <antonioromeu@tecnico.ulisboa.pt>
2020-11-26 11:32:46 +0000

    trying flask

--- web/test.py
+++ web/test.py
@@ -4,33 +4,50 @@ from flask import Flaskimport psycopg2 # Libs postgres
 import psycopg2.extras
 
 ## SGBD configs
-# DB_HOST="db.tecnico.ulisboa.pt”; 
-# DB_USER="istXXXXX" ; 
+# DB_HOST="db.tecnico.ulisboa.pt”;
+# DB_USER="istXXXXX" ;
 # DB_PASSWORD="ZZZZZ" DB_DATABASE=DB_USER;
 # DB_CONNECTION_STRING = "host=%s dbname=%s user=%s password=%s" % (DB_HOST, DB_DATABASE, DB_USER, DB_PASSWORD)
 app = Flask(__name__)
 
-@app.route(‘/’)   # <test.cgi>
-def list_accounts():
-
-    dbConn = None 
-    cursor = None
+@app.route('/')   # <test.cgi>
+def medicos_insert():
+    # dbConn = None
+    # cursor = None
+    title = "medicos insert"
     try:
-        dbConn = psycopg2.connect(DB_CONNECTION_STRING)
-        cursor = dbConn.cursor(cursor_factory = psycopg2.extras.DictCursor)
-        cursor.execute("SELECT * FROM account;")html = #TODO ..... A completar.....
-        # return html #Renders the html string
-        return render_template("index.html", cursor = cursor)  # index.htmlè~/web/templates/ /index.htm
-        return render_template("accounts.html", cursor = cursor) # index.htmlè~/web/templates/ /index.htm
-    except Exception as e:
-        # return e #Renders a page withthe error.
-        return str(e) #Renders a page with the error.
-    finally:
-        cursor.close()
-        dbConn.close()
+        # dbConn = psycopg2.connect(DB_CONNECTION_STRING)
+        # cursor = dbConn.cursor(cursor_factory = psycopg2.extras.DictCursor)
+        if request.method == "POST":
+            ced = request.form("cedula")
+            nome = request.form("nome")
+            esp = request.form("especialidade")
+            return nome
+        
 
-    
-def list_accounts():
-    #TODO         
-    # return <html-string>CGIHandler().run(app)
+# @app.route('/')   # <test.cgi>
+# def list_accounts():
 
+#     dbConn = None
+#     cursor = None
+    # try:
+    #     dbConn = psycopg2.connect(DB_CONNECTION_STRING)
+    #     cursor = dbConn.cursor(cursor_factory = psycopg2.extras.DictCursor)
+    #     cursor.execute("SELECT * FROM account;")html = #TODO ..... A completar.....
+    #     # return html #Renders the html string
+    #     return render_template("index.html", cursor = cursor)  # index.htmlè~/web/templates/ /index.htm
+    #     return render_template("accounts.html", cursor = cursor) # index.htmlè~/web/templates/ /index.htm
+    # except Exception as e:
+    #     # return e #Renders a page with the error.
+    #     return str(e) #Renders a page with the error.
+    # finally:
+    #     cursor.close()
+    #     dbConn.close()
+
+
+# @app.route('/balance')
+# def alter_balance():
+#     try:
+#         return render_template("balance.html", params=request.args)
+#     except Exception as e:
+#         return str(e)
