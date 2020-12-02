83005f7be142d811f76e882e6de294a70e6b761e
antonioromeu <antonioromeu@tecnico.ulisboa.pt>
2020-12-02 12:31:48 +0000

    schema for 4th project altered and create tables from star_schema working

---
+++ projeto4/schema.sql
@@ -0,0 +1,112 @@
+DROP TABLE IF EXISTS prescricao_venda CASCADE;
+DROP TABLE IF EXISTS prescricao CASCADE;
+DROP TABLE IF EXISTS venda_farmacia CASCADE;
+DROP TABLE IF EXISTS analise CASCADE;
+DROP TABLE IF EXISTS consulta CASCADE;
+DROP TABLE IF EXISTS instituicao CASCADE;
+DROP TABLE IF EXISTS concelho CASCADE;
+DROP TABLE IF EXISTS regiao CASCADE;
+DROP TABLE IF EXISTS medico CASCADE;
+
+CREATE TABLE regiao (
+    num_regiao INT NOT NULL,
+    nome VARCHAR(80) NOT NULL CONSTRAINT RI_regiao_1
+        CHECK (nome IN ('Norte', 'Centro', 'Lisboa', 'Alentejo', 'Algarve')),
+    num_habitantes INT NOT NULL,
+    PRIMARY KEY (num_regiao)
+);
+
+CREATE TABLE concelho (
+    num_regiao INT NOT NULL,
+    num_concelho INT NOT NULL,
+    nome VARCHAR(80) NOT NULL,
+    num_habitantes INT NOT NULL,
+    PRIMARY KEY (num_concelho, num_regiao),
+    FOREIGN KEY (num_regiao)
+        REFERENCES regiao(num_regiao) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE instituicao (
+    nome VARCHAR(80) NOT NULL,
+    tipo VARCHAR(20) NOT NULL CONSTRAINT RI_instituicao_1
+        CHECK (tipo IN ('farmacia', 'laboratorio', 'clinica', 'hospital')),
+    num_regiao INT NOT NULL,
+    num_concelho INT NOT NULL,
+    PRIMARY KEY (nome),
+    FOREIGN KEY (num_concelho, num_regiao)
+        REFERENCES concelho(num_concelho, num_regiao) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE medico (
+    num_cedula INT NOT NULL,
+    nome VARCHAR(80) NOT NULL,
+    especialidade VARCHAR(60) NOT NULL,
+    PRIMARY KEY (num_cedula)
+);
+
+CREATE TABLE consulta (
+    num_cedula INT NOT NULL,
+    num_doente INT NOT NULL,
+    data_ DATE NOT NULL CONSTRAINT RI_consulta_1
+        CHECK (EXTRACT(DOW FROM data_) IN (1, 2, 3, 4, 5)),
+    nome_instituicao VARCHAR(80) NOT NULL,
+    PRIMARY KEY (num_cedula, num_doente, data_),
+    FOREIGN KEY (num_cedula)
+        REFERENCES medico(num_cedula) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (nome_instituicao)
+        REFERENCES instituicao(nome) ON DELETE CASCADE ON UPDATE CASCADE,
+    UNIQUE(num_doente, data_, nome_instituicao) -- RI_consulta_2
+);
+
+CREATE TABLE prescricao (
+    num_cedula INT NOT NULL,
+    num_doente INT NOT NULL,
+    data_ DATE NOT NULL,
+    substancia VARCHAR(60) NOT NULL,
+    quantidade INT NOT NULL,
+    PRIMARY KEY (num_cedula, num_doente, data_, substancia),
+    FOREIGN KEY (num_cedula, num_doente, data_)
+        REFERENCES consulta(num_cedula, num_doente, data_) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE analise (
+    num_analise INT NOT NULL,
+    especialidade VARCHAR(60),
+    num_cedula INT NOT NULL,
+    num_doente INT NOT NULL,
+    data_ DATE NOT NULL,
+    data_registo DATE NOT NULL,
+    nome VARCHAR(60) NOT NULL,
+    quant INT NOT NULL,
+    inst VARCHAR(60) NOT NULL,
+    PRIMARY KEY (num_analise),
+    FOREIGN KEY (num_cedula, num_doente, data_)
+        REFERENCES consulta(num_cedula, num_doente, data_) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (inst)
+        REFERENCES instituicao(nome) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE venda_farmacia (
+    num_venda INT NOT NULL UNIQUE,
+    data_registo DATE NOT NULL,
+    substancia VARCHAR(60) NOT NULL,
+    quant INT NOT NULL,
+    preco DECIMAL NOT NULL,
+    inst VARCHAR(80) NOT NULL,
+    PRIMARY KEY (num_venda),
+    FOREIGN KEY (inst)
+        REFERENCES instituicao(nome) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE prescricao_venda (
+    num_cedula INT NOT NULL,
+    num_doente INT NOT NULL,
+    data_ DATE NOT NULL,
+    substancia VARCHAR(60) NOT NULL,
+    num_venda INT NOT NULL UNIQUE,
+    PRIMARY KEY (num_cedula, num_doente, data_, substancia, num_venda),
+    FOREIGN KEY (num_venda)
+        REFERENCES venda_farmacia(num_venda) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (num_cedula, num_doente, data_, substancia)
+        REFERENCES prescricao(num_cedula, num_doente, data_, substancia) ON DELETE CASCADE ON UPDATE CASCADE
+);
--- projeto4/star_schema.sql
+++ projeto4/star_schema.sql
@@ -23,18 +23,17 @@ CREATE TABLE d_instituicao (
     PRIMARY KEY (id_inst),
     FOREIGN KEY (nome)
         REFERENCES instituicao(nome) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (num_regiao)
-        REFERENCES regiao(num_regiao) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (num_concelho)
-        REFERENCES concelho(num_concelho) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (num_concelho, num_regiao)
+        REFERENCES concelho(num_concelho, num_regiao) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE f_presc_venda (
-    id_presc_venda INT NOT NULL,
+    id_presc_venda SERIAL NOT NULL,
     id_medico INT NOT NULL,
     num_doente INT NOT NULL,
     id_data_registo INT NOT NULL,
-    id_inst_substancia INT NOT NULL,
+    id_inst INT NOT NULL,
+    substancia INT NOT NULL,
     quant INT NOT NULL,
     PRIMARY KEY (id_presc_venda),
     FOREIGN KEY (id_presc_venda)
@@ -42,26 +41,105 @@ CREATE TABLE f_presc_venda (
     FOREIGN KEY (id_medico)
         REFERENCES medico(num_cedula) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (id_data_registo)
-        REFERENCES d_tempo(???????) ON DELETE CASCADE ON UPDATE CASCADE,
+        REFERENCES d_tempo(id_tempo) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (id_inst)
         REFERENCES d_instituicao(id_inst) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE f_analise (
-    id_analise INT NOT NULL,
+    id_analise SERIAL NOT NULL,
     id_medico INT NOT NULL,
     num_doente INT NOT NULL,
     id_data_registo INT NOT NULL,
     id_inst INT NOT NULL,
     nome VARCHAR(60) NOT NULL,
     quant INT NOT NULL,
-    PRIMARY KEY (id_analise)
+    PRIMARY KEY (id_analise),
     FOREIGN KEY (id_analise)
         REFERENCES analise(num_analise) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (id_medico)
         REFERENCES medico(num_cedula) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (id_data_registo)
-        REFERENCES d_tempo(??????) ON DELETE CASCADE ON UPDATE CASCADE,
+        REFERENCES d_tempo(id_tempo) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (id_inst)
-        REFERENCES instituicao(??????) ON DELETE CASCADE ON UPDATE CASCADE
+        REFERENCES d_instituicao(id_inst) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+INSERT INTO d_tempo (dia, dia_da_semana, semana, mes, trimestre, ano)
+    SELECT EXTRACT(DAY FROM data_) AS dia,
+        EXTRACT(DOW FROM data_) AS dia_da_semana,
+        EXTRACT(WEEK FROM data_) AS semana,
+        EXTRACT(MONTH FROM data_) AS mes,
+        FLOOR((EXTRACT(MONTH FROM data_) - 1) / 4) + 1 AS trimestre,
+        EXTRACT(YEAR FROM data_) AS ano
+    FROM prescricao
+    ORDER BY dia, dia_da_semana, semana, mes, trimestre, ano;
+
+INSERT INTO d_tempo (dia, dia_da_semana, semana, mes, trimestre, ano)
+    SELECT EXTRACT(DAY FROM data_) AS dia,
+        EXTRACT(DOW FROM data_) AS dia_da_semana,
+        EXTRACT(WEEK FROM data_) AS semana,
+        EXTRACT(MONTH FROM data_) AS mes,
+        FLOOR((EXTRACT(MONTH FROM data_) - 1) / 4) + 1 AS trimestre,
+        EXTRACT(YEAR FROM data_) AS ano
+    FROM analise
+    ORDER BY dia, dia_da_semana, semana, mes, trimestre, ano;
+
+INSERT INTO d_instituicao (nome, tipo, num_regiao, num_concelho)
+    SELECT nome, tipo, num_regiao, num_concelho FROM instituicao;
+
+INSERT INTO f_presc_venda (id_medico, num_doente, id_data_registo, substancia, quant)
+    SELECT num_cedula AS id_medico,
+        num_doente
+    FROM prescricao_venda,
+    SELECT id_data_registo
+    FROM d_tempo
+    WHERE (dia = SELECT(EXTRACT(day FROM prescricao_venda.data_))
+        AND mes = SELECT (EXTRACT(month FROM prescricao_venda.data_))
+        AND ano = SELECT (EXTRACT(year FROM prescricao_venda.data_))),
+    SELECT substancia
+    FROM prescricao_venda,
+    SELECT quant
+    FROM venda_farmacia
+    WHERE num_venda = prescricao_venda.num_venda;
+
+
+    SELECT id_medico, num_doente, id_data_registo, substancia, quant
+    FROM (SELECT id_tempo AS id_data_registo,
+
+        FROM (SELECT id_data_registo
+            FROM d_tempo
+            WHERE (dia = SELECT(EXTRACT(day FROM prescricao_venda.data_))
+            AND mes = SELECT (EXTRACT(month FROM prescricao_venda.data_))
+            AND ano = SELECT (EXTRACT(year FROM prescricao_venda.data_)))
+        )
+    )
+
+        EXTRACT(day FROM ts) AS dia,
+      EXTRACT(dow FROM ts) AS dia_da_semana,
+      EXTRACT(week FROM ts) AS semana,
+      EXTRACT(month FROM ts) AS mes,
+      FLOOR((EXTRACT(month FROM ts) - 1) / 4) + 1 AS trimestre,
+      EXTRACT(year FROM ts) AS ano,
+        CASE
+              WHEN tem_anomalia_redacao
+                THEN 'redacao'
+                ELSE 'traducao'
+          END AS tipo_anomalia,
+      CASE
+        WHEN EXISTS (select *
+              from correcao c
+              where c.anomalia_id = anomalia.id)
+        THEN True
+        ELSE False
+      END AS com_proposta
+      FROM anomalia) AS anomalias NATURAL JOIN
+      incidencia NATURAL JOIN
+      (SELECT id AS item_id,
+        latitude, longitude
+      FROM item) AS items NATURAL JOIN
+      d_utilizador NATURAL JOIN
+      d_lingua NATURAL JOIN
+      d_local NATURAL JOIN
+      d_tempo
+  ORDER BY anomalia_id;
